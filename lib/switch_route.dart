ec943c204e52dfc9aabc959b09d8f043cb1f9d4f
kei1115 <keifukuda1115@gmail.com>
2021-09-14 21:43:46 +0900

    Fix import

--- lib/switch_route.dart
+++ lib/switch_route.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_samples/post.dart';
+import 'package:flutter_samples/page2.dart';
 
 Route createRoute() {
   return PageRouteBuilder(
