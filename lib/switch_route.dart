d2626aba98902e5977e4d0008fb8f04efb4c2d91
kei1115 <keifukuda1115@gmail.com>
2021-09-14 21:42:08 +0900

    Create createRoute function to set animation when switching page

---
+++ lib/switch_route.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_samples/post.dart';
+
+Route createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0); // 変化量(速度)
+      const end = Offset.zero;
+      const curve = Curves.ease; // アニメーションの種類
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
