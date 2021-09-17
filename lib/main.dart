61e327d1780a1e8c0e9c2c7495e0505727612a60
kei1115 <keifukuda1115@gmail.com>
2021-09-18 03:07:26 +0900

    Add Firebase.initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Firebase初期化
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
