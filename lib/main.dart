6b3e0a5f635e9cb2742648c154ee10bb0689b627
kei1115 <keifukuda1115@gmail.com>
2021-09-18 03:13:27 +0900

    Fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_samples/post_page.dart';
+import 'package:flutter_samples/post_page_model.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
