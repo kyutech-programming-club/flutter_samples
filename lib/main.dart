bfd30b9cf11978c6a13465da47d3a422b5d1e0db
kei1115 <keifukuda1115@gmail.com>
2021-09-18 03:11:47 +0900

    Add ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,15 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider<PostPageModel>(
+      create: (_) => PostPageModel(),
+      child : MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
