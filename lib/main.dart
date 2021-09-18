f02bfcfe1b7101324a1d6f502ccaf0518bfeb268
kei1115 <keifukuda1115@gmail.com>
2021-09-18 18:49:35 +0900

    Add import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_samples/firebase.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
