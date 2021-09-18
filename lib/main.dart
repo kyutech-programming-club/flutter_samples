7f1c556469426e6a63ee0193033931c5b68e6faa
kei1115 <keifukuda1115@gmail.com>
2021-09-18 18:24:58 +0900

    Add ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,15 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return  ChangeNotifierProvider<FirebaseModel>(
+        create: (_) => FirebaseModel(),
+        child : MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MyHomePage(title: 'Flutter Demo Home Page'),
+        )
     );
   }
 }
