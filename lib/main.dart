e4484171b9e537fc4f12739b1354069412de07ed
kei1115 <keifukuda1115@gmail.com>
2021-09-18 19:22:31 +0900

    Add an indention

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_samples/firebase.dart';
+import 'package:flutter_samples/firebase_model.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
